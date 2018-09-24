7651c1bcdf2badd7a035a3c9f47a610827f3d782
mgfzemor <mario.ufrgs.inf@gmail.com>
2018-09-24 03:28:54 -0300

    :construction: :lipstick: add Alstar bootstrap template to index page.

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,23 @@
+
+//= require welcome/jquery
+//= require welcome/bootstrap-min
+//= require welcome/wow-min
+//= require welcome/mb-bgndGallery
+//= require welcome/mb-bgndGallery-effects
+//= require welcome/jquery-simple-text-rotator-min
+//= require welcome/jquery-scrollTo-min
+//= require welcome/jquery-nav
+//= require welcome/modernizr-custom
+//= require welcome/grid
+//= require welcome/stellar
+//= require welcome/custom
+
+$('a[href^="#"]').on('click', function(event) {
+var target = $(this.getAttribute('href'));
+if( target.length ) {
+    event.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: target.offset().top
+    }, 1000);
+}
+});
