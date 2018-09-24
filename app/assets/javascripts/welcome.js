53543f9f990bd50df39523f140c870058957447c
mgfzemor <mario.ufrgs.inf@gmail.com>
2018-09-24 04:04:10 -0300

    :fire: :recycle: :ok_hand: generate min JS files.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,11 +2,11 @@
 //= require welcome/jquery
 //= require welcome/bootstrap-min
 //= require welcome/wow-min
-//= require welcome/mb-bgndGallery
+//= require welcome/mb-bgndGallery-min
 //= require welcome/jquery-simple-text-rotator-min
-//= require welcome/jquery-nav
+//= require welcome/jquery-nav-min
 //= require welcome/modernizr-custom
-//= require welcome/grid
+//= require welcome/grid-min
 //= require welcome/custom
 
 $('a[href^="#"]').on('click', function(event) {
