a3ee154dd6772246c007336bd4a29c2799875b3c
mgfzemor <mario.ufrgs.inf@gmail.com>
2018-09-25 05:36:30 -0300

    :sparkles: :construction: :lipstick: add beautiful template to dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class DashboardController < ApplicationController
   def show
-    render params[:page], :layout=> 'dashboard'
+    render params[:page], layout: 'dashboard'
   end
 end
