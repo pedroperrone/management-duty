d2770db12620481e225eb046285120f424a100f9
Pedro Perrone <psperrone@hotmail.com>
2018-09-28 18:29:45 -0300

    Corrects codestyle issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, :role, presence: :true
+  validates :name, :role, presence: true
 end
