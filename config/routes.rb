1868d5729906c159fabf13af87aeb0749935e452
Pedro Perrone <psperrone@hotmail.com>
2018-09-28 18:23:29 -0300

    Create User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Welcome Controller
   root 'welcome#index'
   # Dashboard Controller
