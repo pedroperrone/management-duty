484e746df15c6959e98a37c60a1437056d620749
mgfzemor <mario.ufrgs.inf@gmail.com>
2018-09-29 20:43:32 -0300

    :refactor: set default role as admin, need to be change.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  enum role: [:company_admin, :company_user, :admin]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  before_create :set_default_role
 
-  validates :name, :role, presence: true
+  validates :name, presence: true
+
+  private
+  def set_default_role
+    self.role ||= :company_admin
+  end
+
 end
