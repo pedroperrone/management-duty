541b994146594bb0d3cc01bf6a492ee08460c38a
mgfzemor <mario.ufrgs.inf@gmail.com>
2018-09-29 20:45:39 -0300

    :refactor: :construction: redirect user to dashboard after login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protected
+  def after_sign_in_path_for(resource)  
+    dashboard_index_path || stored_location_for(resource) || root_path
+  end
 end
