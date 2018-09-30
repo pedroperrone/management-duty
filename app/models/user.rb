16e1ee06b03b3b87d955e150b62abda97cc5e597
Mario Figueiró Zemor <mario.figueiro@ufrgs.br>
2018-09-30 18:21:38 -0300

    Bugfix/user model (#12)
    * :recycle: :fire: remove set default role.
    
    * :recycle: :fire: removes enum role
    
    * :recycle: changes type for role input on sign up page.
    
    * :ok_hand: add rubocop changes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  enum role: %i[Company_Administrator]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  before_create :set_default_role
 
   validates :name, :role, presence: true
-
-  private
-
-  def set_default_role
-    self.role ||= :company_admin
-  end
 end
