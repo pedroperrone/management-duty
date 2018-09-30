fca13fd20939d2837317bbac7912113fec150fb2
mgfzemor <mario.ufrgs.inf@gmail.com>
2018-09-29 21:41:09 -0300

    :ok_hand: rubocop changes :ok_hand:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@
 
 class ApplicationController < ActionController::Base
   protected
-  def after_sign_in_path_for(resource)  
+
+  def after_sign_in_path_for(resource)
     dashboard_index_path || stored_location_for(resource) || root_path
   end
 end
