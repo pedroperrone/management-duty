24456cc721e77f4feb0593298aec319f752473ef
mgfzemor <mario.ufrgs.inf@gmail.com>
2018-09-30 17:42:25 -0300

    :recycle: :fire: remove set default role.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  before_create :set_default_role
 
   validates :name, :role, presence: true
-
-  private
-
-  def set_default_role
-    self.role ||= :company_admin
-  end
+  
 end
