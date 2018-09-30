19dec825f4d7f5e2697d5b703b2bd0dcb8e13506
mgfzemor <mario.ufrgs.inf@gmail.com>
2018-09-29 22:01:06 -0300

    :recycle: refactoring code to pass on devise tests.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  enum role: %i[company_admin company_user admin]
+  enum role: %i[Company_Administrator]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   before_create :set_default_role
 
-  validates :name, presence: true
+  validates :name, :role, presence: true
 
   private
 
