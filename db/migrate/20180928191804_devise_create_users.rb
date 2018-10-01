8b1c0270032c22dea1692b3779c0fa1e2febb4bc
mgfzemor <mario.ufrgs.inf@gmail.com>
2018-10-01 16:13:21 -0300

    :recycle: fix error on migrate

--- db/migrate/20180928191804_devise_create_users.rb
+++ db/migrate/20180928191804_devise_create_users.rb
@@ -27,8 +27,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
       t.timestamps null: false
     end
-
-    add_index :users, :id,                   unique: true
+    
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
   end
