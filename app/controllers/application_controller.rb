7b2e7a4e6f357f667d4ca4307d29ca71949a2da2
José Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2018-10-27 18:02:29 -0300

    set_locale as before_action (getting locale from URL get param)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_locale
+  
   protected
 
   def after_sign_in_path_for(resource)
@@ -15,5 +16,13 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:accept_invitation, keys: %i[name role])
+  end
+ 
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
