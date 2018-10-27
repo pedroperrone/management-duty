8e014fb7acbef4be198b6fe691926079995c104b
Pedro Perrone <psperrone@hotmail.com>
2018-10-27 18:24:22 -0300

    Correct shift partitionator name

---
+++ lib/management_duty/shift/partitionator.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module ManagementDuty
+  module Shift
+    class Partitionator
+      def initialize(shift)
+        @shift = shift
+      end
+
+      def partitionate_at(partition_time)
+        unless @shift.contains?(partition_time)
+          raise ManagementDuty::Errors::TimeOutOfShiftRangeError
+        end
+
+        first_new_shift = ::Shift.new(first_shift_params(partition_time))
+        second_new_shift = ::Shift.new(second_shift_params(partition_time))
+        ActiveRecord::Base.transaction do
+          @shift.update!(active: false)
+          first_new_shift.save!
+          second_new_shift.save!
+        end
+      end
+
+      private
+
+      def first_shift_params(partition_time)
+        { starts_at: @shift.starts_at, ends_at: partition_time }
+          .merge(default_params)
+      end
+
+      def second_shift_params(partition_time)
+        { starts_at: partition_time, ends_at: @shift.ends_at }
+          .merge(default_params)
+      end
+
+      def default_params
+        { user: @shift.user, origin_shift: @shift }
+      end
+    end
+end
+end
