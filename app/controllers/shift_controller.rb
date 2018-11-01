929ae82b0dcd917a7047bc446a954244f89eea2d
pacluke <lucasflores77@gmail.com>
2018-10-31 21:56:16 -0300

    keep routes as get

--- app/controllers/shift_controller.rb
+++ app/controllers/shift_controller.rb
@@ -1,17 +1,13 @@
 class ShiftController < ApplicationController
-  # POST /shift
   def create
   end
 
-  # GET /shift/edit
   def edit
   end
 
-  # PUT /shift
   def update
   end
 
-  # DELETE /shift
   def destroy
   end
 end
