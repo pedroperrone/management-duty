e6d6ff564ea784d6486103bb820de456d788df49
pacluke <lucasflores77@gmail.com>
2018-10-31 21:54:08 -0300

    remove reference to superclass

--- app/controllers/shift_controller.rb
+++ app/controllers/shift_controller.rb
@@ -1,21 +1,17 @@
 class ShiftController < ApplicationController
   # POST /shift
   def create
-    super
   end
 
   # GET /shift/edit
   def edit
-    super
   end
 
   # PUT /shift
   def update
-    super
   end
 
   # DELETE /shift
   def destroy
-    super
   end
 end
