a79891ea1d22f9be2f2ee7a2bc0d43e474188c4d
José Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2018-11-02 00:07:58 -0300

    new/create actions

--- app/controllers/shift_controller.rb
+++ app/controllers/shift_controller.rb
@@ -1,5 +1,42 @@
+require 'date'
+
 class ShiftController < ApplicationController
+  def new
+    render 'new', layout: 'dashboard'
+  end
+
   def create
+    start_date = DateTime.new(params["day1"]["starts_at(1i)"].to_i, 
+                            params["day1"]["starts_at(2i)"].to_i,
+                            params["day1"]["starts_at(3i)"].to_i,
+                            params["day1"]["starts_at(4i)"].to_i,
+                            params["day1"]["starts_at(5i)"].to_i)
+
+    end_date = DateTime.new(params["day2"]["ends_at(1i)"].to_i, 
+                            params["day2"]["ends_at(2i)"].to_i,
+                            params["day2"]["ends_at(3i)"].to_i,
+                            params["day2"]["ends_at(4i)"].to_i,
+                            params["day2"]["ends_at(5i)"].to_i)
+
+    user = User.find_by email: params[:user_email]
+
+    if user
+      user_id = user['id'].to_i
+
+      @shift = Shift.new({starts_at: start_date, ends_at: end_date, user_id: user_id})
+      
+      if @shift.save
+        redirect_to '/dashboard'
+
+      else
+        # Erro ao criar turno
+        redirect_to '/shift/new'
+      end
+
+    else
+      # Erro ao obter id do usuario
+      redirect_to '/shift/new'
+    end
   end
 
   def edit
@@ -10,4 +47,5 @@ class ShiftController < ApplicationController
 
   def destroy
   end
+
 end
