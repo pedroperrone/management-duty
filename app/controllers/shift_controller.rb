76e0d118c45fe524bdc7626a886159a89446a582
José Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2018-11-02 23:11:59 -0300

    edit view definition, helpers for setting shift user (from shift_id and from user e-mail) and parsing form dates

--- app/controllers/shift_controller.rb
+++ app/controllers/shift_controller.rb
@@ -2,49 +2,84 @@ require 'date'
 
 class ShiftController < ApplicationController
   before_action :authenticate_admin!
-  before_action :set_user, only: :create
 
-  #
-  def set_user
-    @user = User.find_by_email(params[:user_email])
-    redirect_to shift_new_path if @user.nil?
-  end
+  before_action :set_user_from_email, only: :create
 
-  #
+  before_action :set_user_from_shift, only: :update
+
+  # Views
   def new
+    @shift = Shift.new
+    @shift.starts_at = 0.hours.from_now
+    @shift.ends_at = 1.hours.from_now
+    
     render 'new', layout: 'dashboard'
   end
 
+  def edit
+    @shift = Shift.find_by_id(params[:shift_id])
+    
+    render 'edit', layout: 'dashboard'
+  end
+
+  # CRUD
   def create
-    start_date = DateTime.new(params["day1"]["starts_at(1i)"].to_i, 
-                            params["day1"]["starts_at(2i)"].to_i,
-                            params["day1"]["starts_at(3i)"].to_i,
-                            params["day1"]["starts_at(4i)"].to_i,
-                            params["day1"]["starts_at(5i)"].to_i)
-
-    end_date = DateTime.new(params["day2"]["ends_at(1i)"].to_i, 
-                            params["day2"]["ends_at(2i)"].to_i,
-                            params["day2"]["ends_at(3i)"].to_i,
-                            params["day2"]["ends_at(4i)"].to_i,
-                            params["day2"]["ends_at(5i)"].to_i)
-
-    user_id = @user['id'].to_i
-    @shift = Shift.new({starts_at: start_date, ends_at: end_date, user_id: user_id})
+    @shift = Shift.new(shift_params)
 
     if @shift.save
       redirect_to dashboard_path
     else
+      puts @shift.errors.messages
       redirect_to shift_new_path
     end
   end
 
-  def edit
-  end
 
   def update
+    @shift = Shift.find_by_id(params[:shift_id])
+    
+    if @shift.update(shift_params)
+      redirect_to dashboard_path
+    else
+      puts @shift.errors.messages
+      redirect_to shift_new_path
+    end
+    
   end
 
   def destroy
+  end
+
+  #
+  private
+  def set_user_from_email
+    @user = User.find_by_email(params[:user_email])
+    
+    redirect_to shift_new_path if @user.nil?
+  end
+
+  def set_user_from_shift
+    @user = User.find_by_id(Shift.find_by_id(params[:shift_id]).user_id)
+    
+    redirect_to shift_new_path if @user.nil?
+  end
+
+  #
+  def parse_date_params label
+    # Method for parsing datetime_select date
+    date = DateTime.new(params[:shift][label.to_s + "(1i)"].to_i,
+                        params[:shift][label.to_s + "(2i)"].to_i,
+                        params[:shift][label.to_s + "(3i)"].to_i,
+                        params[:shift][label.to_s + "(4i)"].to_i,
+                        params[:shift][label.to_s + "(5i)"].to_i)
+  end
+
+  def shift_params
+    {
+      :starts_at => parse_date_params(:starts_at),
+      :ends_at => parse_date_params(:ends_at),
+      :user_id => @user.id
+    }
   end
 
 end
