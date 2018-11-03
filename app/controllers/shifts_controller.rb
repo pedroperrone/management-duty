2f72d41f619d5d03459c124fe0dd7164b448dabe
José Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2018-11-03 12:52:03 -0300

    shift -> shifts , using resources on routes

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,6 +1,6 @@
 require 'date'
 
-class ShiftController < ApplicationController
+class ShiftsController < ApplicationController
   before_action :authenticate_admin!
 
   before_action :set_user_from_email, only: :create
